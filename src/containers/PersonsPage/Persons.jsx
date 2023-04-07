1b8af02dfb99da48f92c5123d1bec36c19eaf64d
Maksad <maks14flay@gmail.com>
2023-04-07 20:03:11 +0500

    minor edits

--- src/containers/PersonsPage/Persons.jsx
+++ src/containers/PersonsPage/Persons.jsx
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { API } from '../../api';
 import { getApiResource } from '../../utils/network';
+import { getPersonId, getPersonImg } from '../../services/getPersonsData';
 
 import styles from './Persons.module.css';
 
 export const Persons = () => {
+
     const [personsList, setPersonsList] = useState([]);
 
     const resource = async () => {
-        const res = await getApiResource(API.getPersonsApi());
+        const res = await getApiResource(API.getPersonsPath());
         const list = res.results.map(({ name, url }) => {
-            return { name, url };
+            const id = getPersonId(url);
+            const img = getPersonImg(id);
+            
+            return { name, url, img };
         });
 
         setPersonsList(list);
@@ -25,7 +30,10 @@ export const Persons = () => {
             { personsList.length 
             ? 
                 personsList.map(el => (
-                    <li key={el.name}>{"name " + el.name + ", " + "url " + el.url}</li>
+                    <li key={el.name}>
+                        <span>{"name " + el.name + ", " + "url " + el.url}</span>
+                        <img src={el.img} alt={el.name} />
+                    </li>
                 ))
             :
                 "Список пуст!"
